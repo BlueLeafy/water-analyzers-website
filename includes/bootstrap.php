746d7b2256f3cacdaba56485bdebb356601154cf
BlueLeafy <70538082+BlueLeafy@users.noreply.github.com>
2020-09-12 21:35:07 +0100

    Delete bootstrap.php

--- includes/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Define Base URL */
-define('BASE_URL', 'https://blueleafy.github.io/water-analyzers-website/');
-/* Page GZIP Compression */
-if (substr_count($_SERVER[‘HTTP_ACCEPT_ENCODING],  ‘gzip))
-	ob_start(“ob_gzhandler);
-else
-	ob_start();
-?>
