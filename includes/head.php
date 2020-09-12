2e7094ddf37c5149239fc429e49cf0f71ff8621c
BlueLeafy <BlueLeafy@users.noreply.github.com>
2020-09-12 21:07:53 +0100

    Update

--- includes/head.php
+++ includes/head.php
@@ -5,8 +5,3 @@
 <!-- CSS, Fonts -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
 <link rel="stylesheet" href="<?php echo BASE_URL; ?>css/style.css">
-
-
-
-
-
--- products/index.php
+++ products/index.php
@@ -1,20 +1,58 @@
-<?php
-include '../includes/bootstrap.php';
-?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
 <title>Products</title>
 <meta name="description" content="We manufacture analyzer" />
-<?php
-include '../includes/head.php';
-?>
+<!-- Basic Settings -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- CSS, Fonts -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://blueleafy.github.io/water-analyzers-website/css/style.css">
+
 </head>
 
 <body>
-<?php
-include '../includes/navigation.php';
-?>
+<!-- NAVBAR -->
+<div class="container nav-sticky">
+  <nav class="navbar-wrap">
+    <input type="checkbox" class="toggler">
+    <div class="hamburger">
+      <div></div>
+    </div>
+    <a href="https://blueleafy.github.io/water-analyzers-website/" id="logo-header">
+    <picture class="py-left-2">
+      <source type="image/jpg" srcset="https://blueleafy.github.io/water-analyzers-website/img/logo.jpg">
+      <img src="https://blueleafy.github.io/water-analyzers-website/img/logo.jpg" alt="Immagine"> </picture></a>
+    <ul class="navbar">
+      <li><a href="<https://blueleafy.github.io/water-analyzers-website/index.php"><i class="fas fa-home"></i></a></li>
+      <li><a href="https://blueleafy.github.io/water-analyzers-website/products/"><i class="fas fa-arrow-circle-down"></i> Products</a>
+        <ul>
+          <li><a href="https://blueleafy.github.io/water-analyzers-website/products/product/">Product</a></li>
+          <li><a href="https://blueleafy.github.io/water-analyzers-website/products/product/">Product</a></li>
+            <li><a href="https://blueleafy.github.io/water-analyzers-website/products/landing-page/">Landing Page</a></li>
+          <li><a href="https://blueleafy.github.io/water-analyzers-website/products/">See all</a></li>
+        </ul>
+      </li>
+      <li><a href="https://blueleafy.github.io/water-analyzers-website/applications/"><i class="fas fa-arrow-circle-down"></i> Applications</a>
+        <ul>
+          <li><a href="https://blueleafy.github.io/water-analyzers-website/applications/application/">Application</a></li>
+          <li><a href="https://blueleafy.github.io/water-analyzers-website/applications/application/">Application</a></li>
+          <li><a href="https://blueleafy.github.io/water-analyzers-website/applications/application/">	Application </a></li>
+        </ul>
+      </li>
+      <li><a href="https://blueleafy.github.io/water-analyzers-website/casestudies/">Case Studies</a>
+      </li>
+      <li><a href="https://blueleafy.github.io/water-analyzers-website/about-us/"><i class="fas fa-arrow-circle-down"></i> Company</a>
+       
+      </li>
+      <li><a href="https://blueleafy.github.io/water-analyzers-website/news/">News</a></li>
+      <li><a href="https://blueleafy.github.io/water-analyzers-website/contacts/">Contacts</a></li>
+    </ul>
+  </nav>
+</div>
+
 
 <!-- HEADER -->
 <header class="products-header">
@@ -24,29 +62,21 @@ include '../includes/navigation.php';
 </header>
 <main>
   <div id="center-container" class="margin-1">
-    <section id="gird-products"> <a href="<?php echo BASE_URL; ?>products/product/" class="text-body">
+    <section id="gird-products"> <a href="https://blueleafy.github.io/water-analyzers-website/products/product/" class="text-body">
       <article>
         <div>
           <h2>Analyzer</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil harum minima facilis dolor nisi unde repudiandae? Distinctio molestiae dolores accusamus.</p>
         </div>
       </article>
-      </a> <a href="<?php echo BASE_URL; ?>products/product/" class="text-body">
+      </a> <a href="https://blueleafy.github.io/water-analyzers-website/products/product/" class="text-body">
       <article>
         <div>
           <h2>Analyzer</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil harum minima facilis dolor nisi unde repudiandae? Distinctio molestiae dolores accusamus.</p>
         </div>
       </article>
-      </a> <a href="<?php echo BASE_URL; ?>products/product/" class="text-body">
-      <article>
-        <div>
-          <h2>Analyzer</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil harum minima facilis dolor nisi unde repudiandae? Distinctio molestiae dolores accusamus.</p>
-        </div>
-      </article>
-      </a> 
-        <a href="<?php echo BASE_URL; ?>products/product/" class="text-body">
+      </a> <a href="https://blueleafy.github.io/water-analyzers-website/products/product/" class="text-body">
       <article>
         <div>
           <h2>Analyzer</h2>
@@ -54,7 +84,15 @@ include '../includes/navigation.php';
         </div>
       </article>
       </a> 
-      <a href="<?php echo BASE_URL; ?>products/landing-page/" class="text-body">
+        <a href="https://blueleafy.github.io/water-analyzers-website/products/product/" class="text-body">
+      <article>
+        <div>
+          <h2>Analyzer</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil harum minima facilis dolor nisi unde repudiandae? Distinctio molestiae dolores accusamus.</p>
+        </div>
+      </article>
+      </a> 
+      <a href="https://blueleafy.github.io/water-analyzers-website/products/landing-page/" class="text-body">
       <article>
         <div>
           <h2>Analyzer</h2>
@@ -64,8 +102,32 @@ include '../includes/navigation.php';
       </a>
       </section>
   </div>
-  <?php
-  include '../includes/footer.php';
-  ?>
+  <!-- FOOTER HERE -->
+  <footer id="main-footer" class="bk-black margin-1">
+  <div class="footer flex-row">
+    <article class="py-left-2">
+      <h2 class="text-green">Name Company.</h2>
+      <p>Company Name <br>
+        Street<br>
+        Town<br>
+        Post code<br>
+        Country </p>
+      <p>Website developed by Company Name Ltd.© 2020 All rights reserved.</p>
+      <p><a href="https://blueleafy.github.io/water-analyzers-website/about-us/">Terms and Conditions</a> <br>
+        <a href="https://blueleafy.github.io/water-analyzers-website/about-us/">Privacy Policy and GDPR</a></p>
+    </article>
+    <article class="py-right-2">
+      <p>+00 (0)000 000 0000</p>
+      <p><a href="#">info@water-analyzers-website.com</a></p>
+      <div class="social"> <a href="#"><i class="fab fa-facebook-f fa-2x"></i></a> <a href="#"><i class="fab fa-twitter fa-2x"></i></a> <a href="#"><i class="fab fa-youtube fa-2x"></i></a> <a href="#"><i class="fab fa-linkedin-in fa-2x"></i></a> </div>
+      <picture>
+        <source type="image/png" srcset="https://blueleafy.github.io/water-analyzers-website/img/homepage/logo-small.png">
+        <source type="image/webp" srcset="https://blueleafy.github.io/water-analyzers-website/img/homepage/logo-small.webp">
+        <img src="https://blueleafy.github.io/water-analyzers-website/img/homepage/logo-small.webp" alt="Immagine" class="logo"> </picture>
+    </article>
+  </div>
+</footer>
+</main>
+
 </body>
 </html>
